97c23e4b0fa118243dfa326665a455bb463821a5
kakoehler <ka.koehler@em.uni-frankfurt.de>
2022-11-21 16:12:13 +0100

    a new python script

---
+++ eintritt.py
@@ -0,0 +1,18 @@
+#----------------------------------------------------------
+# Dateiname: eintritt.py
+# Dieses Programm fragt nach dem Alter und gibt für 
+# Personen unter 18 einen anderen Text aus als für 
+# Erwachsene.
+# Autor: Katha
+# Letzte Änderungen: 21.11.2021
+#----------------------------------------------------------
+
+# Eingabe
+print('Wilkommen im Kino!')
+print('Geben Sie bitte Ihr Alter an.')
+alter = int(input('Alter (Jahre): '))
+if alter < 18:
+    print('Dein Ticket kostet 5,00 €')
+else:
+    print('Ihr Ticket kostet 8,50 €')
+print('Viel Spaß!')
