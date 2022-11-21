5668de52bd2ef14181cf1e8775b46e24d245d410
kakoehler <ka.koehler@em.uni-frankfurt.de>
2022-11-21 15:20:10 +0100

    some typical bugs

--- fehler1.py
+++ fehler1.py
@@ -1,11 +1,18 @@
-##---------------------------------------------------------
-## Dateiname: fehler1.py
-## Ein fehlerhafter Programmtext zur Demonstartion.
-## Autor: Katha
-## Letzte Änderung: 21.11.2022
-##---------------------------------------------------------
+#----------------------------------------------------------
+# Dateiname: fehler1.py
+# Ein fehlerhafter Programmtext zur Demonstartion.
+# Eigentlich soll das Programm das Quadrat einer Zahl 
+# ausgeben. Hier wird allerdings nur das Doppelte 
+# ausgegeben. Das ist ein semantischer bzw. logischer 
+# Fehler. Das Programm leistet nicht das, was es leisten 
+# soll.
+# Autor: Katha
+# Letzte Änderung: 21.11.2022
+#----------------------------------------------------------
 
 # fehler1.py
 a = int(input('Zahl: '))
 ergebnis = a * 2
+# debugging: 
+# ergebnis = a ** 2
 print(ergebnis)
--- fehler2.py
+++ fehler2.py
@@ -1,11 +1,19 @@
 #----------------------------------------------------------
 # Dateiname: fehler2.py
 # Ein fehlerhafter Programmtext zur Demonstartion.
+# Eigentlich soll das Programm das Quadrat einer Zahl 
+# ausgeben. Allerdings wird in Zeile 1 der Variablen a 
+# keine Zahl (float()), sondern eine Zeichkette (str()) 
+# zugewiesen. In Zeile 2 führt das dann zu einem 
+# Laufzeitfehler, da ** nicht auf eine Zeichenkette 
+# angewendet werden kann.
 # Autor: Katha
 # Letzte Änderung: 21.11.2022
 #----------------------------------------------------------
 
-#fehler2.py
-a = input('Zahl: ')
-ergebnis = a ** 2
+# fehler2.py
+a = input('Zahl: ')          # 1
+# debugging: 
+# a = float(input('Zahl: '))
+ergebnis = a ** 2            # 2
 print(ergebnis)
